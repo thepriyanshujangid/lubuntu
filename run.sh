7384068a0387effe051973c49899b3efe085c406
priyanshu <priyanshujangid@yahoo.com>
2022-07-18 09:29:10 +0530

    updated

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ sudo -S <<< "p" apt install  nomacs  gedit  gnome-screenshot indicator-multiload
 sudo -S <<< "p" apt purge pidgin* mplayer* xpad* gpicview* update-notifier*  update-manager*  sylpheed* gnome-mpv* synaptic*   -y  
 sudo -S <<< "p" apt autoremove  -y 
 cd ~
-git clone https://github.com/thepriyanshujangid/ssh && cd ssh && cp -r .ssh ~/  && sudo -S <<< "p"  chmod -R 700 ~/.ssh && cd ~ && sudo -S <<< "p" rm ssh  
-git clone  https://github.com/thepriyanshujangid/sdat2img && mv sdat2img /opt
+git clone https://github.com/thepriyanshujangid/ssh && cd ssh && cp -r .ssh ~/  && sudo -S <<< "p"  chmod -R 700 ~/.ssh && cd ~ && sudo -S <<< "p" rm -r ssh  
+git clone  https://github.com/thepriyanshujangid/sdat2img && sudo -S <<< "p" rm -r /sdat2img/.git sudo -S <<< "p" mv sdat2img/* /opt
 
 sudo -S <<< "p" xdg-mime default nomacs.desktop image/gif image/jpeg image/png image/bmp image/x-eps image/x-ico image/x-portable-bitmap image/x-portable-graymap image/x-portable-pixmap image/x-xbitmap image/x-xpixmap 
 lxpanelctl restart
@@ -24,6 +24,7 @@ echo 'xmodmap -e "clear lock"' >>~/.profile
 echo "syndaemon -d -t" >> ~/.profile
 echo "synclient HorizTwoFingerScroll=1" >> ~/.profile
 echo "synclient VertEdgeScroll=0" >> ~/.profile
+echo "export PATH=/opt:$PATH" >> ~/.profile
 cd ~
 source .profile
 sudo -S <<< "p" rm -r lubuntu
